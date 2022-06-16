e204283c1785de52adb55f19d911363d4c9e1bdd
Cirno <cirnoftw@yandex.ru>
2022-06-16 13:59:13 +0300

    Fix delegate re-initialization

--- core/src/main/java/pro/appcraft/lib/auth/AuthFactoryDelegate.kt
+++ core/src/main/java/pro/appcraft/lib/auth/AuthFactoryDelegate.kt
@@ -3,7 +3,7 @@ package pro.appcraft.lib.auth
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleObserver
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -12,18 +12,22 @@ fun Fragment.authFactory(): ReadOnlyProperty<Fragment, AuthFactory> = AuthFactor
 
 private class AuthFactoryDelegate : ReadOnlyProperty<Fragment, AuthFactory> {
     private lateinit var authFactory: AuthFactory
+    private var observer: LifecycleObserver? = null
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): AuthFactory {
-        authFactory = AuthFactory(thisRef)
-        thisRef.lifecycle.addObserver(object : LifecycleEventObserver {
-            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if (!::authFactory.isInitialized) {
+            authFactory = AuthFactory(thisRef)
+        }
+        
+        if (observer == null) {
+            observer = LifecycleEventObserver { _, event ->
                 when (event) {
                     Lifecycle.Event.ON_CREATE -> authFactory.init()
                     Lifecycle.Event.ON_DESTROY -> authFactory.destroy()
                     else -> {}
                 }
-            }
-        })
+            }.also(thisRef.lifecycle::addObserver)
+        }
 
         return authFactory
     }
